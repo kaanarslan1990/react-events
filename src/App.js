ced4ba7df108b8d40c7c978bb8e7897e1d84fa17
kaanarslan1990 <oguzkaanarslan@outlook.com>
2021-09-01 22:38:25 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-// import EventBind from './components/eventBinding/EventBind'
-// import MouseEvents from './components/mouseEvents/MouseEvents'
-// import KeyboardEvents from './components/keyboardEvents/KeyboardEvents'
-import Form from './components/form/Form'
-import './App.css'
+import React,{useState, useEffect} from "react";
+import Header from "./components/Header";
+import List from "./components/List";
+import fs from "./assets/fs.png";
+import aws from "./assets/aws.png";
+import axios from "axios";
 
-const App = () => {
-    return (
-        <div className="App">
-            {/* <h1>Events in React</h1> */}
-            {/* <EventBind /> */}
-            {/* <MouseEvents /> */}
-            {/* <KeyboardEvents /> */}
-            <Form />
-        </div>
-    )
+function App () {
+  const [counter, setCounter] = useState(0)
+  const [img, setImg] = useState()
+  const [studentsList, setStudentList] = useState([]);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res=>setStudentList(res.data))
+  }, []);
+
+  const handleText = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleSearch = () => {
+    setSearch(text);
+  }
+
+  return(
+    <div className="App">
+      <Header img={img}/> {/*içerisine img useState i verdik ve buttonlar sayesinde Header içersinde değişiklik yapıp Header ı render ettirdik.. Header içerisine props yolladık.. props ile değişiklik olduğu için memo olsa da Header render edilecek*/}
+      <p>Counter: {counter}</p>
+      <button onClick={() => setCounter(counter+1)}>Increase</button>
+      <button onClick={() => setImg(fs)}>FS</button>
+      <button onClick={() => setImg(aws)}>AWS</button>
+      <button onClick={() => setImg("")}>Reset</button>
+      <hr/>
+      <input type="text" value={text} onChange={handleText}/>
+      <button onClick={handleSearch}>Search</button>
+      <List students={studentsList}/>
+    </div>
+  )
 }
 
-export default App
+export default App;
